b41135097a42177ad4e8217692c51fe3016fcbfc
Runos <1269367656@qq.com>
2023-09-06 15:16:07 +0800

    更新 boardmix.user.js
    添加提示框提示

--- boardmix.user.js
+++ boardmix.user.js
@@ -2,15 +2,37 @@
 // @name         博思白板自动签到
 // @namespace    https://greasyfork.org/zh-CN/scripts/474533
 // @homepageURL  https://github.com/liuyz0112/UserScript
-// @version      1.1.3
+// @version      1.1.4
 // @description  尝试自动签到博思白板获取AI点数
 // @author       Runos
 // @match        https://boardmix.cn/app/editor/*
 // @license      GPL-3.0 License
 // ==/UserScript==
 
+// 创建一个 div 元素作为提示框的容器
+var message = document.createElement("div");
+// 设置提示框的样式
+message.style.position = "fixed";
+message.style.top = "80%";
+message.style.left = "50%";
+message.style.transform = "translate(-50%, -50%)";
+message.style.background = "#fff";
+message.style.border = "1px solid #ccc";
+message.style.padding = "10px";
+message.style.borderRadius = "5px";
+message.style.boxShadow = "2px 2px 5px rgba(0, 0, 0, 0.3)";
+message.style.zIndex = "9999";
+message.style.display = "none"; // 初始状态下不显示
+// 将提示框添加到页面中
+document.body.appendChild(message);
+
+
 // 读取上次运行时间
 var lastRunTime = localStorage.getItem("boardmix-lastRunTime");
+// 将时间戳转换为 Date 对象
+var date = new Date(parseInt(lastRunTime));
+// 将日期格式化为几点几分的字符串
+var time = ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2);
 
 // 获取当前时间
 var currentTime = new Date().getTime();
@@ -40,6 +62,13 @@ if (!lastRunTime || currentTime - lastRunTime > 24 * 60 * 60 * 1000) {
                 //还没有签到则点击签到
                 if (primaryButton) {
                     primaryButton.click();
+                    // 显示提示框，并在 2 秒后隐藏
+                    message.textContent = "签到成功😀";
+                    message.style.display = "block";
+                    setTimeout(function () {
+                        message.style.display = "none";
+                    }, 2000);
+
                     setTimeout(function () {
                         if (signInButton) {
                             //如果已经签到则关闭界面
@@ -65,5 +94,13 @@ if (!lastRunTime || currentTime - lastRunTime > 24 * 60 * 60 * 1000) {
     clickExpandSign();
     // 保存本次运行时间
     localStorage.setItem("boardmix-lastRunTime", currentTime);
+} else {
+    // 显示提示框，并在 2 秒后隐藏
+    message.textContent = "今天" + time + "已经签到过了哦😀";
+    message.style.display = "block";
+    setTimeout(function () {
+        message.style.display = "none";
+    }, 2000);
+
 }
 
